b2656bf85544cbc428103434dc8da247442736ae
Christopher Whatley <cwhatley@soosoo.local>
2018-12-16 16:35:14 -0500

    Cleaned out some stragglers

--- src/fields/index.js
+++ src/fields/index.js
@@ -1,19 +1,9 @@
-import BooleanField from "./BooleanField";
 import DescriptionField from "./DescriptionField";
-import NumberField from "./NumberField";
 import ObjectField from "./ObjectField";
-import SchemaField from "./SchemaField";
-import StringField from "./StringField";
 import TitleField from "./TitleField";
-import UnsupportedField from "./UnsupportedField";
 
 export default {
-  BooleanField,
   DescriptionField,
-  NumberField,
   ObjectField,
-  SchemaField,
-  StringField,
   TitleField,
-  UnsupportedField,
 };
